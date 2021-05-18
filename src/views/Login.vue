8806b4d9664a01697378989d849786acaf5639ab
ProgramLion <1318248167@qq.com>
2021-05-18 15:33:03 +0800

    feature:删除默认账户

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -27,8 +27,8 @@ export default {
   data() {
     return {
       ruleForm: {
-        username: 'wjl',
-        password: '111111'
+        username: '',
+        password: ''
       },
       rules: {
         username: [
